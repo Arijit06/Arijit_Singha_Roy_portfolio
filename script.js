c580eda45d4998eea652a9ef5ba9dc2e75e0b27c
arijitsingharoy6 <arijitsngharoy6@gmail.com>
2025-02-01 09:52:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Add a simple alert when the form is submitted
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevent the form from submitting
+    alert('Thank you for reaching out! I will get back to you soon.');
+  });
